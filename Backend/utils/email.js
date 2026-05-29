145e566279e053751b82099a3ff7102e477ebc9e
YuktaChaudhari1 <yuktamchaudhari2005@gmail.com>
2026-05-29 15:09:49 +0530

    updated email logic

--- Backend/utils/email.js
+++ Backend/utils/email.js
@@ -5,15 +5,13 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
-  family: 4,
 });
 
 // verify transporter at startup
