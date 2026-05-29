421b5aa9785100318d1d00d5220e2ea8a88d5cab
YuktaChaudhari1 <yuktamchaudhari2005@gmail.com>
2026-05-29 15:00:03 +0530

    updated

--- Backend/utils/email.js
+++ Backend/utils/email.js
@@ -4,11 +4,16 @@ const otp = require("../models/OTP");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  family: 4,
 });
 
 // verify transporter at startup
